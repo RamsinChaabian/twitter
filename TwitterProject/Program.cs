8d886dfe325e88900b62f6798671a7780637527d
Ramsin <ramsin.chaabian@gmail.com>
2019-11-23 11:48:22 +0330

    init and push project

---
+++ TwitterProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TweetSharp;
+
+
+namespace TwitterProject
+{
+    class Program
+    {
+        private static readonly string cusotmerKey = "";
+        private static readonly string customerKeySecret = "";
+        private static readonly string accessToken = "";
+        private static readonly string accessTokenSecret = "";
+
+        private static TwitterService service = new TwitterService(cusotmerKey, customerKeySecret, accessToken, accessTokenSecret);
+        static void  Main(string[] args)
+        {
+            var directMessages =  service.ListDirectMessagesReceived(new ListDirectMessagesReceivedOptions { Count = 10 });
+
+            Console.ReadKey();
+        }
+    }
+}
